5dc275655141f08e23a7a11a4116a39274d2b24a
zayniddindev <zayniddindev@gmail.com>
2022-07-21 10:25:22 +0500

    Longest palindrome substring

--- ts/Longest-Palindrome/index.ts
+++ ts/Longest-Palindrome/index.ts
@@ -1,6 +1,19 @@
-function longestPalindrome(s: string): string {
-    
-  return ";";
+function longestPalindrome(s: string): any {
+  for (let i = 0; i < s.length; i++) {
+    let f = 0, l = s.length - i;
+    for (let j = 0; j < i + 1; j++) {
+      const a = s.substring(f, l);
+      f++; l++;
+      if (isPalindrome(a)) return a;
+    }
+  } return ""
+}
+function isPalindrome(s: string, i = 0): boolean {
+  return ((i = i || 0) < 0 ||i >= s.length >> 1 ||(s[i] == s[s.length - 1 - i] && isPalindrome(s, ++i)));
 }
 
-console.log(longestPalindrome("babad")); //bab
+console.log(
+  longestPalindrome(
+    ""
+  )
+);
