f9f440bdf0637d6397ff9645870b50b2822ee0f6
zayniddindev <zayniddindev@gmail.com>
2022-08-10 16:14:21 +0500

    Implement-strStr problem

---
+++ ts/Implement-strStr/index.ts
@@ -0,0 +1,6 @@
+//https://leetcode.com/problems/implement-strstr/
+function strStr(haystack: string, needle: string): number {
+    return haystack.indexOf(needle)
+}
+
+console.log(strStr("hello", ""));
