81660d86d2e1039e848a4fbd87619ea893cc96d5
zayniddindev <zayniddindev@gmail.com>
2022-08-10 16:15:01 +0500

    Letter-Combinations problem

---
+++ js/Letter-Combinations/index.js
@@ -0,0 +1,13 @@
+const letters = {
+  2: ["a", "b", "c"],
+  3: ["d", "e", "f"],
+  4: ["g", "h", "i"],
+  5: ["j", "k", "l"],
+  6: ["m", "n", "o"],
+  7: ["p", "q", "r", "s"],
+  8: ["t", "u", "v"],
+  9: ["x", "y", "z", "w"],
+};
+function letterCombinations(digits) {}
+  
+//9
